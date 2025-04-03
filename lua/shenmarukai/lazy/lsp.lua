c27e174a8277cf84b6b0d0ba7e920ae25df79889
Shane Mulcahy <shanemulc@comcast.net>
2025-04-02 23:15:48 -0400

    Update.

--- lua/shenmarukai/lazy/lsp.lua
+++ lua/shenmarukai/lazy/lsp.lua
@@ -19,6 +19,7 @@ return {
             formatters_by_ft = {
             }
         })
+        vim.lsp.inlay_hint.enable()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
