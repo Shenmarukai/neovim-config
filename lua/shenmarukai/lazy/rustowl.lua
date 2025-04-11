20e52a41954a0002039ca4695ad4a4ad709cac1f
Shenmarukai <shanemulc@comcast.net>
2025-04-11 15:01:45 -0400

    More Rust Owl and Twilight changes.

--- lua/shenmarukai/lazy/rustowl.lua
+++ lua/shenmarukai/lazy/rustowl.lua
@@ -5,16 +5,23 @@ return {
     version = '*', -- Latest stable version
     dependencies = { "neovim/nvim-lspconfig" },
     lazy = false, -- This plugin is already lazy
-    opts = {
-        auto_attach = true, -- Auto attach the RustOwl LSP client when opening a Rust file
-        auto_enable = true, -- Enable RustOwl immediately when attaching the LSP client
-        idle_time = 500, -- Time in milliseconds to hover with the cursor before triggering RustOwl
-        client = {
-            on_attach = function(_, buffer)
-                vim.keymap.set('n', '<leader>o', function()
-                    require('rustowl').toggle(buffer)
-                end, { buffer = buffer, desc = 'Toggle RustOwl' })
-            end
-        }, -- LSP client configuration that gets passed to `vim.lsp.start`
-    },
+
+    config = function()
+        require('rustowl').setup({})
+
+        local opts = {
+            auto_attach = true, -- Auto attach the RustOwl LSP client when opening a Rust file
+            auto_enable = true, -- Enable RustOwl immediately when attaching the LSP client
+            idle_time = 500, -- Time in milliseconds to hover with the cursor before triggering RustOwl
+            client = {
+                on_attach = function(_, buffer)
+                    vim.keymap.set('n', '<leader>o', function()
+                        require('rustowl').toggle(buffer)
+                    end, { buffer = buffer, desc = 'Toggle RustOwl' })
+                end
+            }, -- LSP client configuration that gets passed to `vim.lsp.start`
+        }
+
+        require('rustowl.opts').setup(opts)
+    end
 }
--- lua/shenmarukai/lazy/twilight.lua
+++ lua/shenmarukai/lazy/twilight.lua
@@ -1,24 +1,31 @@
 -- Twilight
 
 return {
-  "folke/twilight.nvim",
-  opts = {
-    dimming = {
-      alpha = 0.25, -- amount of dimming
-      -- we try to get the foreground from the highlight groups or fallback color
-      color = { "Normal", "#ffffff" },
-      term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
-      inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
-    },
-    context = 0, -- amount of lines we will try to show around the current line
-    treesitter = true, -- use treesitter when available for the filetype
-    -- treesitter is used to automatically expand the visible text,
-    -- but you can further control the types of nodes that should always be fully expanded
-    expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
-      "_declaration",
-      "_definition",
-      "_statement"
-    },
-    exclude = {}, -- exclude these filetypes
-  },
+    "folke/twilight.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+        require('twilight').setup({})
+
+        local opts = {
+            dimming = {
+                alpha = 0.25, -- amount of dimming
+                -- we try to get the foreground from the highlight groups or fallback color
+                color = { "Normal", "#ffffff" },
+                term_bg = "#000000", -- if guibg=NONE, this will be used to calculate text color
+                inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+            },
+            context = 0, -- amount of lines we will try to show around the current line
+            treesitter = true, -- use treesitter when available for the filetype
+            -- treesitter is used to automatically expand the visible text,
+            -- but you can further control the types of nodes that should always be fully expanded
+            expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+                "_declaration",
+                "_definition",
+                "_statement"
+            },
+            exclude = {}, -- exclude these filetypes
+        }
+
+        require('twilight').setup(opts)
+    end
 }
