4cc13e1f70a19e885260912b6423e9fb80b8ec1e
Shane Mulcahy <shanemulc@comcast.net>
2025-08-05 23:31:00 -0400

    Update.

--- lua/shenmarukai/lazy/lint.lua
+++ lua/shenmarukai/lazy/lint.lua
@@ -8,10 +8,43 @@ return {
     },
     {
         "mfussenegger/nvim-lint",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
         config = function()
             local lint = require("lint")
 
+            lint.linters.cppcheck.args = {
+                "--enable=warning,style,performance,information",
+                function()
+                    if vim.bo.filetype == "cpp" then
+                        return "--language=c++"
+                    else
+                        return "--language=c"
+                    end
+                end,
+                "--inline-suppr",
+                "--quiet",
+                function()
+                    if vim.fn.isdirectory("build") == 1 then
+                        return "--cppcheck-build-dir=build"
+                    else
+                        return nil
+                    end
+                end,
+                function()
+                    -- Add suppressions-list if .cppcheck-suppress exists
+                    if vim.fn.filereadable(".cppcheck-suppress") == 1 then
+                        return "--suppressions-list=.cppcheck-suppress"
+                    else
+                        return nil
+                    end
+                end,
+                "--template={file}:{line}:{column}: [{id}] {severity}: {message}",
+            }
+
             lint.linters_by_ft = {
+                make = { "checkmake" },
                 rust = { "bacon" },
                 cpp = { --[["cpplint",]] "cppcheck" },
                 c = { --[["cpplint",]] "cppcheck" },
@@ -21,7 +54,7 @@ return {
                 typescriptreact = { "biomejs" },
             }
 
-            vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+            vim.api.nvim_create_autocmd({ "InsertLeave", "BufReadPost", "BufNewFile" }, {
                 callback = function()
                     lint.try_lint()
                 end,
