9e10aae22e17a1263efab983a77391bd718d710a
Shenmarukai <shanemulc@comcast.net>
2025-08-13 08:42:14 -0400

    Added gh.nvim

--- lua/shenmarukai/lazy/gp.lua
+++ lua/shenmarukai/lazy/gp.lua
@@ -1,7 +1,7 @@
 -- GP
 
 return {
-    "Shenmarukai/gp.nvim",
+    "robitx/gp.nvim",
     config = function()
         require( "gp" ).setup({
 	        providers = {
@@ -10,7 +10,6 @@ return {
 	        		endpoint = "https://api.openai.com/v1/chat/completions",
 	        	    secret = { "cat", "/home/shane/.openai/.gp.nvim.key" },
 	        	},
-
 	        	copilot = {
                     disable = true,
 	        		endpoint = "https://api.githubcopilot.com/chat/completions",
@@ -20,12 +19,10 @@ return {
 	        			"cat ~/.config/github-copilot/apps.json | sed -e 's/.*oauth_token...//;s/\".*//'",
 	        		},
 	        	},
-
 	        	ollama = {
                     disable = true,
 	        		endpoint = "http://localhost:11432/v1/chat/completions",
 	        	},
-
 	        	anthropic = {
                     disable = true,
 	        		endpoint = "https://api.anthropic.com/v-1/messages",
@@ -33,25 +30,64 @@ return {
 	        	},
 	        },
             agents = {
-		        {
+                --[[ChatGPT4.1]] {
+                    provider = "openai",
+			        name = "ChatGPT4.1",
+			        chat = true,
+			        command = false,
+			        -- string with model name or table with model name and parameters
+			        model = {
+                        model = "gpt-4.1",
+                        temperature = 0.0,
+                        max_tokens = 32768,
+                        top_p = 0
+                    },
+			        -- system prompt (use this to specify the persona/role of the AI)
+			        system_prompt = require( "gp.defaults" ).chat_system_prompt,
+		        },
+		        --[[ChatGPT5]] {
+		        	provider = "openai",
+		        	name = "ChatGPT5",
+		        	chat = true,
+		        	command = false,
+		        	-- string with model name or table with model name and parameters
+		        	model = {
+                        model = "gpt-5",
+                        reasoning_effort = "high",
+                        verbosity = "high",
+                        summary = "detailed"
+                    },
+		        	-- system prompt (use this to specify the persona/role of the AI)
+		        	system_prompt = require( "gp.defaults" ).code_system_prompt,
+		        },
+		        --[[CodeGPT4.1]] {
 		        	provider = "openai",
 		        	name = "CodeGPT4.1",
 		        	chat = false,
 		        	command = true,
 		        	-- string with model name or table with model name and parameters
-		        	model = { model = "gpt-4.1", temperature = 0.0, top_p = 0 },
+		        	model = {
+                        model = "gpt-4.1",
+                        temperature = 0.0,
+                        max_tokens = 32768,
+                        top_p = 0
+                    },
 		        	-- system prompt (use this to specify the persona/role of the AI)
-		        	system_prompt = require("gp.defaults").code_system_prompt,
+		        	system_prompt = require( "gp.defaults" ).code_system_prompt,
 		        },
-		        {
+		        --[[CodeGPT5]] {
 		        	provider = "openai",
 		        	name = "CodeGPT5",
 		        	chat = false,
 		        	command = true,
 		        	-- string with model name or table with model name and parameters
-		        	model = { model = "gpt-5" },
+		        	model = {
+                        model = "gpt-5",
+                        reasoning_effort = "high",
+                        verbosity = "high"
+                    },
 		        	-- system prompt (use this to specify the persona/role of the AI)
-		        	system_prompt = require("gp.defaults").code_system_prompt,
+		        	system_prompt = require( "gp.defaults" ).code_system_prompt,
 		        },
 	        },
         })
